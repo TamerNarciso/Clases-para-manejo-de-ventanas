1a638b21a3494861ca3acdafe693a71703e3a926
TamerNarciso <120230178+TamerNarciso@users.noreply.github.com>
2023-05-24 19:09:55 -0600

    Add files via upload

---
+++ test 2-1.py
@@ -0,0 +1,9 @@
+from Programas.base import Base
+class Test(Base):
+    def initialize(self):
+        print("Initializing program...")
+    def update(self):
+        pass
+
+# instantiate this class and run the program
+Test().run()
